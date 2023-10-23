f3f7606280f5e976fc6421a6c6a9d563b2bc29c0
Serhii Tymoshenko <sergei.timoshenko@pm.me>
2023-10-23 06:18:11 +0200

    Added constants

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,7 @@
+package utils
+
+public const val TICK_TIME: Long = 1;
+public const val TICKS_IN_HOUR: Int = 60;
+public const val HOURS_IN_DAY: Int = 24;
+public const val TICKS_IN_DAY: Int =
+    HOURS_IN_DAY * TICKS_IN_HOUR;
